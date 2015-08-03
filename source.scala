c25b81f64111fe6fa7dc47015cdc97a7a41c16e6
cloudera <sebastien.diaz@gmail.com>
2015-08-03 13:19:21 -0700

    add lsa

--- source.scala
+++ source.scala
@@ -7,11 +7,12 @@ import breeze.linalg.{DenseMatrix => BDenseMatrix, DenseVector => BDenseVector, 
 
 val csv = sc.textFile("file:/home/cloudera/data/training_set_rel3.tsv")
 // split / clean data
-val headerAndRows = csv.map(line => line.split("\t").map(_.trim))
+val headerAndRows = csv.map(line => line.split("\t").map(_.trim).map(_.replaceAll("(^\"|\"$)","")))
 // get header
 val header = headerAndRows.first
 // filter out header (eh. just check if the first val matches the first header name)
 val data = headerAndRows.filter(_(0) != header(0))
+
 //data.foreach(println)
 
 var essay1=data.filter(a=>a(1).equals("1"))
@@ -24,3 +25,21 @@ var essay7=data.filter(a=>a(1).equals("7"))
 var essay8=data.filter(a=>a(1).equals("8"))
 
 val stopWords = sc.broadcast(ParseWikipedia.loadStopWords("deps/lsa/src/main/resources/stopwords.txt")).value
+var lemmatized=essay1.map(s=> (s(0),ParseWikipedia.plainTextToLemmas(s(2), stopWords, ParseWikipedia.createNLPPipeline())))
+val filtered = lemmatized.filter(_._2.size > 1)
+
+val numTerms = 1000
+val (termDocMatrix, termIds, docIds, idfs) = ParseWikipedia.termDocumentMatrix(filtered, stopWords, numTerms, sc)
+
+val mat = new RowMatrix(termDocMatrix)
+val k = 200 // nombre de valeurs singuliers à garder
+val svd = mat.computeSVD(k, computeU=true)
+val topConceptTerms = RunLSA.topTermsInTopConcepts(svd, 10, 10, termIds)
+val topConceptDocs = RunLSA.topDocsInTopConcepts(svd, 10, 10, docIds)
+    for ((terms, docs) <- topConceptTerms.zip(topConceptDocs)) {
+              println("Concept terms: " + terms.map(_._1).mkString(", "));
+              println("Concept docs: " + docs.map(_._1).mkString(", "));
+              println();
+           }
+
+
